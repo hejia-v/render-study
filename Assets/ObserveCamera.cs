65866c04a73d39a60b6cec7023a21b3b72cb65a7
hejia <1464946535@qq.com>
2018-01-12 22:47:31 +0800

    up

--- Assets/ObserveCamera.cs
+++ Assets/ObserveCamera.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ObserveCamera : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+public class ObserveCamera : MonoBehaviour
 {
     public bool lockCursor;
     public float mouseSensitivity = 10;
     public Transform target;
     public float dstFromTarget = 2;
+    public float zoomSpeed = 1;
     public Vector2 pitchMinMax = new Vector2(-40, 85);
 
     public float rotationSmoothTime = .12f;
@@ -25,10 +26,12 @@ public class ObserveCamera : MonoBehaviour, IDragHandler, IPointerUpHandler, IPo
 
     void Update()
     {
+        float distance = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        dstFromTarget += distance;
         transform.position = target.position - transform.forward * dstFromTarget;
     }
 
-    private void UpdateCamera()
+    public void UpdateCamera()
     {
         yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
         pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
@@ -40,22 +43,5 @@ public class ObserveCamera : MonoBehaviour, IDragHandler, IPointerUpHandler, IPo
         transform.position = target.position - transform.forward * dstFromTarget;
     }
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        UpdateCamera();
-    }
 
-    public void OnDrag(PointerEventData eventData)
-    {
-        //float s = Input.GetAxis("Mouse X");
-        //Debug.Log("22    " + s);
-        //Debug.Log("-----------" );
-
-        UpdateCamera();
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        UpdateCamera();
-    }
 }
---
+++ Assets/TouchHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchHandler : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    public ObserveCamera observeCamera;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void UpdateCamera()
+    {
+        if (observeCamera!=null)
+        {
+            observeCamera.UpdateCamera();
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        UpdateCamera();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        UpdateCamera();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        UpdateCamera();
+    }
+}
